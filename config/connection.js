e5e69015ec7c2b09e7f50f8307caebbe54af7052
miraisekai <116041477+miraisekai@users.noreply.github.com>
2023-04-17 16:23:48 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social_db', {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social_db",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
